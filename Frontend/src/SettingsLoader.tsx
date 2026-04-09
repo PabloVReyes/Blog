7972a481f89b56b631cc23199ddca84b751d4e9a
PabloVReyes <pablovazquezreyes@outlook.com>
2026-04-09 09:21:36 -0600

    US-003: Tipar Actions components US-005: Tipar mapeos de formularios US-006: Tipar RemotePaginatedSelect

--- Frontend/src/SettingsLoader.tsx
+++ Frontend/src/SettingsLoader.tsx
@@ -1,32 +1,12 @@
 import { useEffect } from "react"
 import { useSettingStore } from "./features"
-import { getApiAssetUrl } from "@/utils/apiUrl"
 
 export const SettingsLoader = () => {
     const loadSettings = useSettingStore((state) => state.loadSettings)
-    const favicon = useSettingStore((state) => state.favicon)
 
     useEffect(() => {
         loadSettings()
     }, [loadSettings])
-
-    useEffect(() => {
-        if (!favicon) return
-
-        const faviconUrl = getApiAssetUrl(favicon)
-
-        let link =
-            document.querySelector<HTMLLinkElement>("link[rel='icon']") ||
-            document.createElement("link")
-
-        link.type = "image/x-icon"
-        link.rel = "icon"
-        link.href = faviconUrl
-
-        if (!link.parentNode) {
-            document.head.appendChild(link)
-        }
-    }, [favicon])
 
     return null
 }
